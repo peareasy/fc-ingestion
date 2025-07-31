a247e1ff3fbdc0517a331f009cc5ca00994af928
Sebastian Specht <sebastian.a.specht@gmail.com>
2025-07-31 14:06:28 +0200

    tried to have process.env

--- infrastructure/bin/fc-ingestion.ts
+++ infrastructure/bin/fc-ingestion.ts
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
+/// <reference types="node" />
 import * as cdk from "aws-cdk-lib";
 import { FcIngestionStack } from "../lib/fc-ingestion-stack";
 import { GitHubActionsOidcStack } from "../lib/github-actions-oidc-stack";
 
 const app = new cdk.App();
-
-// Get MongoDB URI from environment variable
-const mongodbUri = process.env.MONGODB_URI;
-if (!mongodbUri) {
-  throw new Error("MONGODB_URI environment variable is required");
-}
 
 // Deploy OIDC stack first (if needed)
 // if (true) {
@@ -27,5 +22,5 @@ new FcIngestionStack(app, "FcIngestionStack", {
     account: "120099621477",
     region: "eu-west-1",
   },
-  mongodbUri,
+  mongodbUri: process.env.MONGODB_URI || "",
 });
